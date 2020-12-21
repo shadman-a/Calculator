7aa8317b3ba73bae945571477bb9047a0c3dc358
shadman-a <shadman@outlook.com>
2020-12-21 14:14:01 -0500

    Done

--- src/main/java/calculator/InteractiveCalculator.java
+++ src/main/java/calculator/InteractiveCalculator.java
@@ -1,5 +1,10 @@
 package calculator;
 
+import calculator.operations.AddOperation;
+import calculator.operations.DivideOperation;
+import calculator.operations.MultiplyOperation;
+import calculator.operations.SubtractOperation;
+
 import java.util.Scanner;
 
 public class InteractiveCalculator {
@@ -12,7 +17,33 @@ public class InteractiveCalculator {
         int numberB = scanner.nextInt();
         System.out.println("Give me a operation please");
         scanner.nextLine();
-        String Operation = Scanner.nextLine();
-        System.out.println(scanner.nextInt());
+        String Operation = scanner.nextLine();
+
+        int result = 0;
+        if (Operation.equals("add")) {
+            AddOperation add = new AddOperation();
+            add.setA(numberA);
+            add.setB(numberB);
+            result = add.getResult();
+        }else if (Operation.equals("subtract")){
+            SubtractOperation subtract = new SubtractOperation();
+            subtract.setA(numberA);
+            subtract.setB(numberB);
+            result = subtract.getResult();
+        }else if (Operation.equals("multiply")){
+            MultiplyOperation multiply = new MultiplyOperation();
+            multiply.setA(numberA);
+            multiply.setB(numberB);
+            result = multiply.getResult();
+        }else if (Operation.equals("divide")){
+            DivideOperation divide = new DivideOperation();
+            divide.setA(numberA);
+            divide.setB(numberB);
+            result = divide.getResult();
+        }else{
+            System.out.println("Did not understand operation");
+        }
+
+        System.out.println("The result is " + result);
     }
 }
--- src/main/java/calculator/operations/AddOperation.java
+++ src/main/java/calculator/operations/AddOperation.java
@@ -4,7 +4,15 @@ final public class AddOperation {
     int a = 0;
     int b = 0;
 
-    int getResult() {
+    public void setA(int input){
+        this.a = input;
+    }
+
+    public void setB(int input){
+        this.b = input;
+    }
+
+    public int getResult() {
         return this.a + this.b;
     }
 }
